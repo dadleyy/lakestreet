a8d96eb6be48279c998029babbdb332620a6d392
Danny Hadley <dadley@sizethreestudios.com>
2014-02-26 23:19:36 -0500

    more inital foundation - move sass and js into src dir, added ngtemplates

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,17 +2,19 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-smash');
+  grunt.loadNpmTasks('grunt-angular-templates');
 
   grunt.initConfig({
 
     pkg: grunt.file.readJSON('package.json'),
 
-    clean: ["public/js"],
+    clean: ["public/js", "public/css"],
 
     smash: {
       build: {
-        src: "src/lakestreet.js",
+        src: "src/js/lakestreet.js",
         dest: "public/js/app.js"
       }
     },
@@ -20,14 +22,29 @@ module.exports = function(grunt) {
     compass: {
       build: {
         options: {
-          sassDir: 'sass',
+          sassDir: 'src/sass',
           cssDir: 'public/css'
         }
+      }
+    },
+
+    uglify: {
+      build: {
+        files: {
+          'public/js/app.min.js': ['public/js/app.js']
+        }
+      }
+    },
+
+    ngtemplates:  {
+      app: {
+        src: 'src/html/**.html',
+        dest: 'public/js/templates.js'
       }
     }
 
   });
 
-  grunt.registerTask('default', ['clean','smash','compass']);
+  grunt.registerTask('default', ['clean','smash','compass','uglify','ngtemplates']);
 
 };
--- public/index.php
+++ public/index.php
@@ -3,7 +3,9 @@
 <head>
   <meta charset="utf-8" />
   <title>lakestreet drive | interactive</title>
+  <link href="/css/app.css" type="text/css" rel="stylesheet" />
 </head>
 <body>
+  <script src="/js/app.js" type="text/javascript"></script>
 </body>
 </html>
