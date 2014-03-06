ef495456dc09c051307fdda1e162ffbee5d96884
danny hadley <dhadley@merchantwarehouse.com>
2014-03-06 17:34:50 -0500

    using locking title in middle of screen approach

--- public/index.php
+++ public/index.php
@@ -7,11 +7,10 @@
   <link href="/fonts/index.css?v=<?= $cache_break ?>" type="text/css" rel="stylesheet" />
   <link href="/css/app.css?v=<?= $cache_break ?>" type="text/css" rel="stylesheet" />
 </head>
-<body class="preload">
+<body class="preload" ld-scroll-broadcaster>
   <div ld-load-splash></div>
   <div class="page">
-    <div ng-view>
-    </div> 
+    <div ng-view></div> 
   </div>
   <div ld-footer></div>
   <script src="/js/app.js?v=<?= $cache_break ?>" type="text/javascript"></script>
--- src/js/directives/album.js
+++ src/js/directives/album.js
@@ -14,6 +14,18 @@ ld.directive('ldAlbum', [function() {
     }],
     link: function($scope, element, attr) { 
       $scope.playing = false;  
+
+      $scope.albumFade = function(page_top, bounding_box) {
+        var window_mid = (window.innerHeight * 0.5) + page_top,
+            distance = window_mid - bounding_box.top,
+            opacity;
+
+        if(distance > 0) 
+          distance = 0;
+
+        opacity = 1 + (distance * 0.025);
+        element.css({opacity: opacity});
+      };
     }
   };
 
