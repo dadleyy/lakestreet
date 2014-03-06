8655a138554b15975ef93006eefedbf7dbb24b08
danny hadley <dhadley@merchantwarehouse.com>
2014-03-06 12:39:27 -0500

    fixing strange flicker bug

--- public/index.php
+++ public/index.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html ng-app="lakestreet">
 <head>
+  <?php $cache_break = sha1(time()); ?>
   <meta charset="utf-8" />
   <title>lakestreet dive | interactive</title>
-  <link href="/fonts/index.css" type="text/css" rel="stylesheet" />
-  <link href="/css/app.css" type="text/css" rel="stylesheet" />
+  <link href="/fonts/index.css?v=<?= $cache_break ?>" type="text/css" rel="stylesheet" />
+  <link href="/css/app.css?v=<?= $cache_break ?>" type="text/css" rel="stylesheet" />
 </head>
 <body class="preload">
   <div ld-load-splash></div>
@@ -13,7 +14,7 @@
     </div> 
   </div>
   <div ld-footer></div>
-  <script src="/js/app.js" type="text/javascript"></script>
-  <script src="/js/templates.min.js" type="text/javascript"></script>
+  <script src="/js/app.js?v=<?= $cache_break ?>" type="text/javascript"></script>
+  <script src="/js/templates.min.js?v=<?= $cache_break ?>" type="text/javascript"></script>
 </body>
 </html>
