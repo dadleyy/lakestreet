7ba8374bcfbfd9976e12a9d4e08fa0ccf4600baf
danny hadley <dadley@sizethreestudios.com>
2014-03-14 16:08:33 -0400

    worked on collapsing and expanding twitter feed and twitter items

--- src/js/directives/twitter_feed.js
+++ src/js/directives/twitter_feed.js
@@ -1,14 +1,45 @@
-ld.directive('ldTwitterFeed', [function() {
+ld.directive('ldTwitterFeed', ['MenuManager', function(MenuManager) {
+
+  function TwitterFeed($scope) {
+    $scope.expanded = false;
+    this.menu_id = null;
+    this.$scope = $scope;
+  };
+
+  TwitterFeed.prototype.toggle = function(evt) {
+    evt.stopPropagation();
+
+    this.$scope.expanded = !this.$scope.expanded;
+
+    if(this.$scope.expanded)
+      this.menu_id = MenuManager.push(angular.bind(this,this.close));
+    else
+      MenuManager.pop(this.menu_id);
+
+    return this.$scope.expanded;
+  };
+
+  TwitterFeed.prototype.close = function() {
+    this.$scope.expanded = false;
+  };
+
+  TwitterFeed.prototype.isExpanded = function() {
+    return this.$scope.expanded === true;
+  };
+
+  TwitterFeed.$inject = ['$scope'];
 
   return {
     restrict: 'EA',
     replace: true,
     templateUrl: 'directives.twitter_feed',
     scope: { tweets: '=' },
-    link: function($scope, $element, $attrs) {
+    controller: TwitterFeed,
+    link: function($scope, $element, $attrs, twitterFeed) {
       $scope.expanded = false;
-      $scope.expand = function() { 
-        $scope.expanded = !$scope.expanded;
+
+      $scope.expand = function(evt) { 
+        return twitterFeed.toggle(evt);
       };
     }
   };
--- src/js/filters/twitter_text.js
+++ src/js/filters/twitter_text.js
@@ -3,13 +3,13 @@ ld.filter('twitterText', ['$sce', function($sce) {
   var MENTION_REGEX = /@(\w+)/g,
       MENTION_REPLACE = "<a href=\"https://twitter.com/$1\" target=\"_blank\">$1</a>",
       HASH_REGEX = /#(\w+)/g,
-      HASH_REPLACE = "<a href=\"http://twitter.com/search?q=%23$1\">#$1</a>",
-      LINK_REGEX = /http:(.*)/g,
-      LINK_REPLACE = "fuh";
+      HASH_REPLACE = "<a href=\"http://twitter.com/search?q=%23$1\" target=\"_blank\">#$1</a>",
+      LINK_REGEX = /http([s]?):\/\/([^\ \)$]*)/g,
+      LINK_REPLACE = "<a href=\"http://$2\" target=\"_blank\">$2</a>";
 
   function parseEntities(text) {
     var links= text.replace(LINK_REGEX, LINK_REPLACE),
-        mentions = text.replace(MENTION_REGEX, MENTION_REPLACE),
+        mentions = links.replace(MENTION_REGEX, MENTION_REPLACE),
         hashes = mentions.replace(HASH_REGEX, HASH_REPLACE);
 
     return hashes;
