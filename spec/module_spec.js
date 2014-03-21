1faac23c1d368d6940cffc7fd494032cfc071eb3
Danny Hadley <dadley@sizethreestudios.com>
2014-03-21 00:21:16 -0400

    moving route spec to separate file

--- spec/module_spec.js
+++ spec/module_spec.js
@@ -2,39 +2,8 @@ describe('the module', function() {
   
   beforeEach(module('lakestreet'));
 
-  var $location, $rootScope, $route, $httpBackend, album_ids;
-
-  var prep = ['$location', '$rootScope', '$route', '$httpBackend', 'ALBUM_IDS', function() {
-    $location = arguments[0];
-    $rootScope = arguments[1];
-    $route = arguments[2];
-    $httpBackend = arguments[3];
-    album_ids = arguments[4];
-  }];
-
-  beforeEach(inject(prep));
-
-  beforeEach(function() {
-    for(var i = 0; i < album_ids.length; i++ ) {
-      $httpBackend.expectGET(/api\/camp.*/).respond(200, {});
-    }
-    $httpBackend.expectGET(/api\/town.*/).respond(200, []);
-    $httpBackend.expectGET(/api\/twit.*/).respond(200, []);
-
-    $location.path('/home');
-    $rootScope.$digest();
-
-    $httpBackend.flush();
-  });
-
-  afterEach(function() {
-    $httpBackend.verifyNoOutstandingExpectation();
-    $httpBackend.verifyNoOutstandingRequest();
-  });
-
   it('should exist', function() {
-    expect($location.path()).toBe('/home');
-    expect($route.current.templateUrl).toBe('views.home');
+    expect(true).toBe(true);
   });
 
 });
